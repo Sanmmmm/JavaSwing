7e7ed5cfd5a2fcd7a6d609211245c64b720aaa70
Sanmmmm <samngof@gmail.com>
2024-08-26 19:24:47 -0300

    Add files via upload

---
+++ CALCULADORAE/src/main/java/com/mycompany/calculadorae/CALCULADORAE.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorae;
+
+/**
+ *
+ * @author sanmg
+ */
+public class CALCULADORAE {
+
+    public static void main(String[] args) {
+        TelaPrincipal a = new TelaPrincipal();
+        a.setVisible(true);
+             
+    }
+}
